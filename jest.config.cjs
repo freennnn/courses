5de1d12c65990514d5cbdc00e295b87b82b9fd37
alex.pranevich <freennnn@gmail.com>
2023-08-05 14:15:20 -0700

    feature: add Jest support for Ts and ESM

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,14 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts', '**/*.test.js'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  preset: "ts-jest/presets/default-esm",
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
+  coveragePathIgnorePatterns : [
+    'd.ts' 
+  ],
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
   coverageThreshold: {
     global: {
