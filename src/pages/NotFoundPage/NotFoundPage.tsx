cb83f49fb5534e180dbed9b811d650b814b7c2a6
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-08-08 14:08:25 -0500

    RF-22: feat - add base layout for NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom';
+
+import './NotFoundPage.scss';
+
 const NotFoundPage = () => {
   return (
-    <>
-      <h2>404 Not Found</h2>
-    </>
+    <div className='not-found-page'>
+      <div className='container'>
+        <div className='not-found-page-content'>
+          <div className='not-found-page-content__image'>404</div>
+          <div className='not-found-page-content__info'>
+            <h2 className='not-found-page-content__title'>This page could not be found</h2>
+            <p className='not-found-page-content__text'>
+              You can either stay and chill here, or go back to the beginning.
+            </p>
+            <Link className='not-found-page-content__link' to='/'>
+              BACK TO HOME
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
