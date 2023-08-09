632a29d53d6eda5126c3f0fb8337d6e6b2631c35
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-08-09 10:18:40 -0500

    RF-22: feat - add styles and some layout for NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -7,10 +7,13 @@ const NotFoundPage = () => {
     <div className='not-found-page'>
       <div className='container'>
         <div className='not-found-page-content'>
-          <div className='not-found-page-content__image'>404</div>
+          <div className='not-found-page-content__image'>
+            <p className='not-found-page-content__image__text'>404</p>
+          </div>
+          <div className='not-found-page-content__separator'></div>
           <div className='not-found-page-content__info'>
-            <h2 className='not-found-page-content__title'>This page could not be found</h2>
-            <p className='not-found-page-content__text'>
+            <h2 className='not-found-page-content__info__title'>This page could not be found</h2>
+            <p className='not-found-page-content__info__text'>
               You can either stay and chill here, or go back to the beginning.
             </p>
             <Link className='not-found-page-content__link' to='/'>
