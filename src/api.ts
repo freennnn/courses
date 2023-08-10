e55dac6fdfec0ecf1ef569448f3e8357d9b8f2f6
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-08-10 10:45:53 -0500

    RF-12: feat - add login function

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,59 @@
-export default class Loader {
-  baseLink = 'mc.us-central1.gcp.commercetools.com';
-  options = {};
+import {
+  ClientBuilder,
+  Client,
+  HttpMiddlewareOptions,
+  PasswordAuthMiddlewareOptions,
+} from '@commercetools/sdk-client-v2';
 
-  constructor(baseLink?: string, options?: Record<string, string>) {
-    if (baseLink) {
-      this.baseLink = baseLink;
-    }
-    if (options) {
-      this.options = options;
-    }
+import {
+  createApiBuilderFromCtpClient,
+  ApiRoot,
+  CustomerSignin,
+} from '@commercetools/platform-sdk';
+
+const projectKey = 'rs-final';
+
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: 'https://api.us-central1.gcp.commercetools.com',
+  fetch,
+};
+
+const passwordAuthMiddlewareOptions: PasswordAuthMiddlewareOptions = {
+  host: 'https://auth.us-central1.gcp.commercetools.com',
+  projectKey,
+  credentials: {
+    clientId: 'HdvN8dBxrWuKQrvKMBDClG29',
+    clientSecret: 'oBAc24vfn5Bhnr4Xa0u4MAkvyqgWzdcp',
+    user: {
+      username: 'example@example.com',
+      password: 'your-password',
+    },
+  },
+  scopes: ['manage_customers:rs-final'],
+  fetch,
+};
+
+const client: Client = new ClientBuilder()
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withPasswordFlow(passwordAuthMiddlewareOptions)
+  .build();
+
+const getApiRoot: () => ApiRoot = () => {
+  return createApiBuilderFromCtpClient(client);
+};
+
+export const loginUser = async (loginRequest: CustomerSignin) => {
+  try {
+    const response = await getApiRoot()
+      .withProjectKey({ projectKey })
+      .login()
+      .post({ body: loginRequest })
+      .execute();
+
+    // eslint-disable-next-line no-console
+    console.log('Logged In Customer:', response.body);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Login Error:', error);
   }
-}
+};
