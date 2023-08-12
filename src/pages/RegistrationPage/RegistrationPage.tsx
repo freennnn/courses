d99988f74971359fbe4729b6368d0a21b356d430
Your Name <you@example.com>
2023-08-12 16:06:04 +0300

    feat: add registration page

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,9 +1,30 @@
-const RegistrationPage = () => {
+import './RegistrationPage.scss';
+import RegistrationForm from '../../components/LoginForm/RegistrationForm';
+
+function RegistrationPage() {
   return (
-    <>
-      <p>Registration Page</p>
-    </>
+    <div className='reg'>
+      <div className='reg__wrapper'>
+        <div className='reg__decor'>
+          <a className='reg__main-link' href='/'></a>
+          <p>
+            <img src='src/assets/images/logo.svg' alt='logo' width='115' height='115' />
+          </p>
+          <h2 className='reg__subtitle'>Nice to meet you :)</h2>
+          <p className='reg__text'>Just register to join with us</p>
+        </div>
+        <section className='reg__inner'>
+          <div className='reg__flex'>
+            <h1>Registration</h1>
+            <a className='reg__link' href='/login'>
+              Already have account
+            </a>
+          </div>
+          <RegistrationForm />
+        </section>
+      </div>
+    </div>
   );
-};
+}
 
 export default RegistrationPage;
