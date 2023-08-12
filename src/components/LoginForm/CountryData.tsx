48902d1f715635869101452939a0a9f4f4f24694
Your Name <you@example.com>
2023-08-12 15:50:33 +0300

    feat: add country-data

---
+++ src/components/LoginForm/CountryData.tsx
@@ -0,0 +1,8 @@
+const countries = [
+  { id: 'US', descr: 'USA', postCode: /^\d{5}$/ },
+  { id: 'AUS', descr: 'Australia', postCode: /^\d{5}$/ },
+  { id: 'ES', descr: 'Spain', postCode: /^\d{5}$/ },
+  { id: 'DE', descr: 'Germany', postCode: /^\d{5}$/ },
+  { id: 'BLR', descr: 'Belarus', postCode: /^\d{6}$/ },
+];
+export default countries;
