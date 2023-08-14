9b6394c47995fd800ac96b9173a4a67745f72760
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-08-14 10:01:38 -0500

    fix: remove mock functions from App

--- src/App.tsx
+++ src/App.tsx
@@ -5,49 +5,10 @@ import MainPage from './pages/MainPage/MainPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
-import { signUp, signIn } from './api/api';
 
 import './App.scss';
 
-//temporary user for testing
-const customer = {
-  email: 'user12',
-  password: 'password',
-  firstName: 'John',
-  lastName: 'Doe',
-  dateOfBirth: '2000-10-12',
-  addresses: [{ country: 'US' }],
-};
-
 const App = () => {
-  //temporary function for testing
-  const handleSignUp = () => {
-    signUp(customer)
-      .then((response) => {
-        // eslint-disable-next-line no-console
-        console.log('Sign Up Response:', response);
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error('Sign Up Error:', error);
-      });
-  };
-
-  const handleSignIn = () => {
-    signIn({
-      email: 'example@example.com',
-      password: 'your-password',
-    })
-      .then((response) => {
-        // eslint-disable-next-line no-console
-        console.log('Sign Ip Response:', response);
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error('Sign Up Error:', error);
-      });
-  };
-
   return (
     <>
       <Routes>
@@ -58,8 +19,6 @@ const App = () => {
         <Route path='register' element={<RegistrationPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
-      <button onClick={handleSignUp}>Sign up</button>
-      <button onClick={handleSignIn}>Sign in</button>
     </>
   );
 };
