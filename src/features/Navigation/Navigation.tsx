50f1ab5aede56e12f6ad92e235b60a4f635df26e
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-08-17 09:56:51 -0500

    RE-30: fix - move login button from main page to navigation

--- src/features/Navigation/Navigation.tsx
+++ src/features/Navigation/Navigation.tsx
@@ -1,8 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+
+import { AppContext } from '../../contexts/AppContext';
 
 const Navigation = () => {
+  const { isSignedIn } = useContext(AppContext);
+
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    if (isSignedIn) {
+      navigate('/');
+    } else {
+      navigate('/login');
+    }
+  };
   return (
     <>
+      <button onClick={handleLoginClick}>Login</button>
       <Outlet />
     </>
   );
