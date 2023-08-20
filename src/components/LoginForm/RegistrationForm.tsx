8def2120d90358dfae92581c7b80cdf81a7a26bb
iisakjanova <87137513+iisakjanova@users.noreply.github.com>
2023-08-20 12:20:40 -0500

    fix: resolve merging errors

--- src/components/LoginForm/RegistrationForm.tsx
+++ src/components/LoginForm/RegistrationForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
