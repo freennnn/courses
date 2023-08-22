30cbe9144477b938946f777b78305a14d0160d4d
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-08-21 19:05:39 -0500

    fix: add route and some layout for User Profile page

---
+++ src/pages/UserProfilePage/UserProfilePage.tsx
@@ -0,0 +1,15 @@
+import './UserProfilePage.scss';
+
+const UserProfilePage = () => {
+  return (
+    <>
+      <div className='user-profile-page'>
+        <div>
+          <h1 className='user-profile-page__title'>User Profile</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserProfilePage;
