f1e662577bbfc69e58f5906b4e205170c28cdca6
Your Name <you@example.com>
2023-08-28 17:11:01 +0300

    feat: add user profile page

--- src/pages/UserProfilePage/UserProfilePage.tsx
+++ src/pages/UserProfilePage/UserProfilePage.tsx
@@ -1,15 +1,27 @@
+import { Link } from 'react-router-dom';
 import './UserProfilePage.scss';
 
-const UserProfilePage = () => {
+function UserProfilePage() {
   return (
-    <>
-      <div className='user-profile-page'>
-        <div>
-          <h1 className='user-profile-page__title'>User Profile</h1>
+    <div className='user' id='user'>
+      <div className='user__wrapper'>
+        <div className='user__decor'>
+          <Link className='user__main-link' to={'/'}></Link>
+          <p className='user__logo'></p>
+          <h2 className='user__subtitle'>Nice to meet you :)</h2>
+          <p className='user__text'>Check infomation about you</p>
         </div>
+        <section className='user__inner'>
+          <div className='user__flex'>
+            <h1>User Profile</h1>
+            <Link className='reg__link' to={'/register'}>
+              Create a new account
+            </Link>
+          </div>
+        </section>
       </div>
-    </>
+    </div>
   );
-};
+}
 
 export default UserProfilePage;
