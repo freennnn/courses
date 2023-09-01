1292915f304303426ed86fd762df8937a7f28f03
alex.pranevich <freennnn@gmail.com>
2023-08-31 21:19:37 -0700

    feat: basic Product Detail layout

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -2,6 +2,8 @@ import { useLocation } from 'react-router-dom';
 // import { SliderType }
 import Slider from '@/components/Slider/Slider';
 import { SliderItemType } from '@/components/Slider/SliderItem';
+import Button from '@/components/Button/Button';
+import { ButtonType, ButtonBackgroundColor } from '@/components/Button/Button.types';
 import { ProductItem } from 'types';
 import './ProductDetailPage.scss';
 
@@ -21,10 +23,31 @@ export default function ProductDetailPage() {
     slider = <h3>Ups, product does not have images</h3>;
   }
 
+  let fullPrice = 0;
+  let discountedPrice = 0;
+
+  if (product?.price) {
+    fullPrice = product.price / 100;
+  }
+
+  if (product?.discount) {
+    discountedPrice = fullPrice - Number(product.discount) / 100;
+  }
+
   return (
     <div className='product-detail'>
-      <h1>3 beers for the price of 2! The more you drink - the more you save!</h1>
-      {slider}
+      <div className='product-detail__container '>
+        <div className='product-detail__content-container'>
+          <div className='product-detail__text-content'>
+            <h3 className='product-detail__title'>{product?.name['en-US']}</h3>
+            <p className='product-detail__description'>{product?.description?.['en-US']}</p>
+            <Button type={ButtonType.contained} color={ButtonBackgroundColor.accented}>{`Buy for $${
+              discountedPrice > 0 ? discountedPrice : fullPrice
+            }`}</Button>
+          </div>
+          {slider}
+        </div>
+      </div>
     </div>
   );
 }
