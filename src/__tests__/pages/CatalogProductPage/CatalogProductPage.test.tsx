61f8ab2c955fcfee7bc499c3a15fca57435c9139
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-09-11 09:41:47 -0500

    fix: make a try to mock a function

--- src/__tests__/pages/CatalogProductPage/CatalogProductPage.test.tsx
+++ src/__tests__/pages/CatalogProductPage/CatalogProductPage.test.tsx
@@ -1,23 +1,28 @@
+/* eslint no-var: off */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { BrowserRouter } from 'react-router-dom';
 
 import { jest } from '@jest/globals';
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import fetchMock from 'jest-fetch-mock';
 import CatalogProductPage from 'pages/CatalogProductPage/CatalogProductPage';
 
-const getProductsListMock = jest.fn();
-jest.mock('pages/CatalogProductPage/helpers.ts', () => {
-  const originalModule = jest.requireActual<typeof import('pages/CatalogProductPage/helpers.ts')>(
-    'pages/CatalogProductPage/helpers.ts',
-  );
+//https://github.com/sodatea/vite-jest/tree/main/packages/vite-jest#limitations-and-differences-with-commonjs-tests
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+var getProductsListMock;
+
+jest.mock('pages/CatalogProductPage/helpers.ts', () => {
+  getProductsListMock = jest.fn(() => Promise.resolve());
   return {
-    __esModule: true, // Use it when dealing with esModules
-    ...originalModule,
     getProductsList: getProductsListMock,
   };
 });
+
+jest.mock('features/CategoryList/CategoryList', () => () => 'CategoryList');
 
 describe('CatalogProductPage', () => {
   beforeEach(() => {
@@ -25,16 +30,21 @@ describe('CatalogProductPage', () => {
   });
 
   it('filters products based on selectedYear', () => {
-    render(
-      <BrowserRouter>
-        <CatalogProductPage />
-      </BrowserRouter>,
-    );
+    act(() => {
+      render(
+        <BrowserRouter>
+          <CatalogProductPage />
+        </BrowserRouter>,
+      );
+    });
 
-    // Simulate selecting a year from a dropdown, e.g., "2023"
-    const yearDropdown = screen.getByLabelText('Year:'); // Adjust the selector as needed
-    fireEvent.change(yearDropdown, { target: { value: '2023' } });
-
-    expect(getProductsListMock.mock.calls.length).toBe(1);
+    act(() => {
+      // Simulate selecting a year from a dropdown, e.g., "2023"
+      const yearDropdown = screen.getByLabelText('Year:'); // Adjust the selector as needed
+      fireEvent.change(yearDropdown, { target: { value: '2023' } });
+    });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    expect(getProductsListMock.mock.calls.length).toBe(2);
   });
 });
