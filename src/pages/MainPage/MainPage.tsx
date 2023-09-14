0164eef0ec8e917f49b088f8d4ed2a95222e6d76
massaracsh7 <fatum7@gmail.com>
2023-09-14 20:12:41 +0300

    refactor: change banner

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,7 +6,7 @@ export default function MainPage() {
       <div className='main-feature'>
         <div>
           <p className='main__banner'>
-            Write the magic word ten and get a 10% discount on all products
+            Write the magic word <span>ten</span> and get a 10% discount on all products
           </p>
           <h1>Hello fellow wizards!</h1>
           <p>Let&#39; sell some courses and movies and get rich fast</p>
